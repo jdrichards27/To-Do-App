f9bbec0b0190fdefb6d3d507addfecc847aab60c
jdrichards <jdrichards027@gmail.com>
2025-08-03 16:25:45 -0400

    Implement add new todo feature

--- web.py
+++ web.py
@@ -3,6 +3,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state['new_todo'] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
 st.title("To-Do List")
 st.subheader("This is a task management application")
 st.write("Tasks to be completed:")
@@ -11,5 +16,6 @@ st.write("Tasks to be completed:")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo...")
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
